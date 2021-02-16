114e330008904be7ee8afb04fc2b8962cf3563c8
teddy <teddyjh@protonmail.com>
2021-02-16 08:22:37 +0100

    add path to screenshot files
    We create a variable t o hold the path to the Desktop where the
    screenshots are located.

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from pathlib import Path
 
 def main():
     p = Path.home()
-    print(f"Path: {p}")
+    #screenshotPath = p + "/Desktop/"
+    print(p)
+    pnew = p.joinpath('/Users/teddy/', './Desktop/')
+    print(pnew)
+    for filenames in os.listdir(pnew):
+        print(filenames)
+
 
 if __name__ == '__main__':
     main()
+
