cec609cae6f6930553feaac29306799acef218e4
teddy <teddyjh@protonmail.com>
2021-02-16 08:47:39 +0100

    add a todo and change variable pnew -> pathDesktop

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ def main():
     p = Path.home()
     #screenshotPath = p + "/Desktop/"
     print(p)
-    pnew = p.joinpath('/Users/teddy/', './Desktop/')
-    print(pnew)
+    pathDesktop = p.joinpath('/Users/teddy/', './Desktop/')
+    print(pathDesktop)
 
     # List Comprehensions
-    screenshots = [file for file in os.listdir(pnew) if "screenshot" in file.lower()]
+    screenshots = [file for file in os.listdir(pathDesktop) if "screenshot" in file.lower()]
     print(screenshots)
+
+    # TODO: Move the found files to /Users/teddy/Pictures/screenshots
 
 if __name__ == '__main__':
     main()
