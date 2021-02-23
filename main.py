4aab4b36b5a10fdecbcc0a1aa0721579d1079aa0
teddy <teddyjh@protonmail.com>
2021-02-23 07:58:25 +0100

    clean the code a bit
    We clean the code a bit. We remove some imports that are not in use and
    move the functions around a bit.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-from pathlib import Path
+# main.py
 
 from getPathToUserDesktop import getPathToUserDesktop
 from getPathToScreenshotFiles import getListOfScreenshotFiles
@@ -8,18 +7,16 @@ from moveScreenshotFiles import moveScreenshotFiles
 
 def main():
     # Get the absolute path to the users desktop
-    p = Path.home()
     pathDesktop = getPathToUserDesktop()
-
-    # Get a list of all the screenshots at ~/Desktop/
-    screenshots = getListOfScreenshotFiles(pathDesktop)
 
     # Get the path to ~/Pictures/screenshots
     pathScreenshots = getPathToScreenshotsDirectory()
+    
+    # Get a list of all the screenshots at ~/Desktop/
+    screenshots = getListOfScreenshotFiles(pathDesktop)
 
     #  Move all screenshots by renaming them
     moveScreenshotFiles(pathDesktop, pathScreenshots, screenshots)
 
 if __name__ == '__main__':
     main()
-
