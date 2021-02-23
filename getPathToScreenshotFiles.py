c902207245c434eb655c6c962e23377b8eb75fbb
teddy <teddyjh@protonmail.com>
2021-02-23 08:01:50 +0100

    add TODO: remove dependency of 'os'-module
    We add a TODO where we want to remove the dependency of the 'os'-module
    in the function were we use them.

--- getPathToScreenshotFiles.py
+++ getPathToScreenshotFiles.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+# TODO: Remove dependency of 'os'-module. Only pathlib / Path
+
 def getListOfScreenshotFiles(pathToDesktop):
     # List Comprehension to get all the screenshot files
     return [file for file in os.listdir(pathToDesktop) if "screenshot" in file.lower()]
