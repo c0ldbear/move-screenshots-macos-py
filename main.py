346f8b22cd6fa0482ac1496fd782b1b3095e05f6
teddy <teddyjh@protonmail.com>
2021-02-23 07:21:20 +0100

    remove 'debug' prints that is not neccessary

--- main.py
+++ main.py
@@ -4,18 +4,13 @@ from pathlib import Path
 
 def main():
     p = Path.home()
-    #screenshotPath = p + "/Desktop/"
-    print(p)
     pathDesktop = p.joinpath(p, './Desktop/')
-    print(pathDesktop)
 
     # List Comprehensions
     screenshots = [file for file in os.listdir(pathDesktop) if "screenshot" in file.lower()]
-    print(screenshots)
 
     # TODO: Move the found files to /Users/teddy/Pictures/screenshots
     pathScreenshots = p.joinpath(p, './Pictures/screenshots/')
-    print(pathScreenshots)
     for file in screenshots:
         os.rename(p.joinpath(pathDesktop, file), p.joinpath(pathScreenshots, file))
 
